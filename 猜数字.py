892a66fe9c21e3ba03c5a81dc96dc05691f24a5d
Lok <42785367+weilelin123@users.noreply.github.com>
2022-12-21 15:09:09 -0800

    Add files via upload

---
+++ 猜数字.py
@@ -0,0 +1,22 @@
+import random
+
+num = random.randint(0, 100)
+#num = 20
+#输入指定范围
+
+a = 0
+b = 100
+
+for i in range(1, 4):
+    I_num = int(input("Please type in a integer between（{}-{}): ".format(a, b)))
+    if I_num == num:
+        print("Your are right! The right answer is: {}. You guess {} times".format(I_num, i))
+        break
+    elif I_num > num:
+        b = I_num
+        print("This is your {} guess, need to guess smaller".format(i))
+    elif I_num  < num:
+        a = I_num
+        print("This is your {} guess, need to guess bigger".format(i))
+else:
+    print("Times up! The program is end. The right answer is: {}".format(num))
